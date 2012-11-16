be5235ebb55aad97020be72c5f6fccf203dced7b
Takano Akio <aljee@hyper.cx>
2012-11-16 11:08:09 +0900

    Weak: fix a segfault with GHC 7.6

--- FRP/Ordrea/Weak.hs
+++ FRP/Ordrea/Weak.hs
@@ -1,8 +1,10 @@
-{-# LANGUAGE MagicHash, UnboxedTuples #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -Wall #-}
 
 -- WARNING
--- This file causes a segfault if interpreted with ghci.
+-- This file causes a segfault if interpreted with ghci < 7.6.
 -- If you use ghci, compile this file first and then
 -- load the .o file to ghci.
 
@@ -19,7 +21,16 @@ mkWeakWithIORef (IORef (STRef ref)) v (Just fin) = IO $ \s ->
   case mkWeak# ref v fin s of
     (# s1, w #) -> (# s1, Weak w #)
 mkWeakWithIORef (IORef (STRef ref)) v Nothing = IO $ \s ->
-  case mkWeak# ref v (unsafeCoerce# 0#) s of
+  case mkWeakNoFinalizer# ref v s of
     (# s1, w #) -> (# s1, Weak w #)
+
+# if __GLASGOW_HASKELL__ < 706
+mkWeakNoFinalizer#
+  :: MutVar# RealWorld a
+  -> v
+  -> State# RealWorld
+  -> (# State# RealWorld, Weak# v #)
+mkWeakNoFinalizer# key val s = mkWeak# key val (unsafeCoerce# 0#) s
+# endif
 
 -- vim: sw=2 ts=2 sts=2
