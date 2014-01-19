47d464356a9132ac05d9f60bf43f322935b93520
Takano Akio <aljee@hyper.cx>
2014-01-19 12:31:29 +0900

    add withPrevE, dropE and dropWhileE

--- FRP/Ordrea.hs
+++ FRP/Ordrea.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+{-# OPTIONS_GHC -Wall -fno-warn-missing-signatures #-}
 module FRP.Ordrea
   (
   -- * Basic types
@@ -6,7 +8,7 @@ module FRP.Ordrea
 
   -- * External events
   , ExternalEvent
-  , newExternalEvent, triggerExternalEvent, listenToExternalEvent
+  , newExternalEvent, triggerExternalEvent
 
   -- * Events
   , generatorE, filterE, stepClockE, dropStepE, eventFromList
@@ -14,6 +16,7 @@ module FRP.Ordrea
   , accumE, scanAccumE, scanAccumEM
   , mapMaybeE, justE, flattenE, expandE, externalE
   , takeWhileE, delayE
+  , withPrevE, dropE, dropWhileE
 
   -- * Switchers
   , joinDD, joinDE, joinDS
@@ -24,6 +27,7 @@ module FRP.Ordrea
 
   -- * Discretes
   , scanD, changesD, preservesD, delayD
+  , stepperD
 
   -- * Signal-event functions
   , eventToSignal, signalToEvent, applySE
@@ -38,4 +42,60 @@ module FRP.Ordrea
   , OrderingViolation (..)
   ) where
 
+import Control.Applicative
+
 import FRP.Ordrea.Base
+import UnitTest
+
+-- Derived functions
+
+stepperD :: a -> Event a -> SignalGen (Discrete a)
+stepperD initial evt = scanD initial (const <$> evt)
+
+withPrevE :: a -> Event a -> SignalGen (Event (a, a))
+withPrevE initial evt = scanE (initial, err) $ upd <$> evt
+  where
+    upd new (old, _) = (new, old)
+    err = error "FRP.Ordrea.withPrevE: bug: prehistoric element"
+
+dropE :: Int -> Event a -> SignalGen (Event a)
+dropE n evt = justE <$> mapAccumE n (f <$> evt)
+  where
+    f occ 0 = (0, Just occ)
+    f _ k = (k', Nothing)
+      where !k' = k - 1
+
+dropWhileE :: (a -> Bool) -> Event a -> SignalGen (Event a)
+dropWhileE p evt = justE <$> mapAccumE True (f <$> evt)
+  where
+    f occ True
+      | p occ = (True, Nothing)
+    f occ _ = (False, Just occ)
+
+----------------------------------------------------------------------
+-- tests
+
+_unitTest = runTestTT $ test
+  [ test_withPrevE
+  , test_dropE
+  , test_dropWhileE
+  ]
+
+test_withPrevE = do
+  r <- networkToList 3 $ do
+    evt <- withPrevE 0 =<< eventFromList [[1,2], [], [3 :: Int]]
+    return $ eventToSignal evt
+  r @?= [[(1,0), (2,1)], [], [(3,2)]]
+
+test_dropE = do
+  r <- networkToList 3 $ do
+    evt <- dropE 1 =<< eventFromList [[1,2], [], [3 :: Int]]
+    return $ eventToSignal evt
+  r @?= [[2], [], [3]]
+
+test_dropWhileE = do
+  r <- networkToList 3 $ do
+    evt <- dropWhileE (<=2)
+      =<< eventFromList [[1,2], [], [3,4 :: Int]]
+    return $ eventToSignal evt
+  r @?= [[], [], [3,4]]
